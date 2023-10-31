17586ce8091270e2c69af5df0860eeec4a462e19
MahithaVudutha <vuduthamahitha@gmail.com>
2023-10-31 16:12:12 -0500

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var zebraRouter = require('./routes/zebra');
-var boardRouter = require('./routes/board');
+//var boardRouter = require('./routes/board');
 //var chooseRouter = require('./routes/choose');
 
 var app = express();
